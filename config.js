2a59907a69c6dc2918dc5e7b854ce537df685c49
malin50 <115778204+malin50@users.noreply.github.com>
2024-08-11 06:12:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/kxQ6d5K/City-Scapes7.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am maliya bot I am alive now!❤️",
 };
